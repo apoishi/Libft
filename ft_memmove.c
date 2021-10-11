851592154a38a49b979b756f78156b4089e62d2b
Ariane Padilha Oishi <arianeoishi@Arianes-MacBook-Air.local>
2021-10-11 15:50:43 -0300

    final commit

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,33 +6,23 @@
 /*   By: apadilha <apadilha@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 18:09:23 by apadilha          #+#    #+#             */
-/*   Updated: 2021/10/09 22:43:14 by apadilha         ###   ########.fr       */
+/*   Updated: 2021/10/11 15:47:45 by apadilha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *s1, const void *s2, size_t n)
 {
-	unsigned char		*cdest;
-	const unsigned char	*csrc;
-	size_t				i;
+	char	*temp;
 
-	cdest = (unsigned char *)dest;
-	csrc = (unsigned char *)src;
-	i = 0;
-	if (!cdest && !csrc)
-		return (NULL);
-	if (cdest > csrc)
-		while (n-- > 0)
-			cdest[n] = csrc[n];
-	else
+	temp = (char *)malloc(n * sizeof(char));
+	if (temp == NULL)
 	{
-		while (i < n)
-		{
-			cdest[i] = csrc[i];
-			i++;
-		}
+		return (NULL);
 	}
-	return (dest);
+	ft_memcpy(temp, s2, n);
+	ft_memcpy(s1, temp, n);
+	free(temp);
+	return (s1);
 }
